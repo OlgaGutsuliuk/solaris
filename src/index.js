54852add9b9181e478ec2271eb8253d3aa17f721
Olga Hutsulyuk <o.hutsulyuk@gmail.com>
2023-01-05 13:20:23 +0200

    my-web-solaris

---
+++ src/index.js
@@ -0,0 +1,26 @@
+$(".models__our").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+  dots: false,
+  vertical: true
+});
+$(".models-destop").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+  dots: false
+});
+$(".share-wrapper").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: false,
+  infinite: true,
+  arrows: false,
+  cssEase: "linear",
+  focusOnSelect: true
+});
